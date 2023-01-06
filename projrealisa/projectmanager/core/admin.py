35ccbab379b361676930e1ceecf09b319784d7aa
jakin6 <70150840+jakin6@users.noreply.github.com>
2023-01-06 15:29:33 +0200

    initial commit

--- projrealisa/projectmanager/core/admin.py
+++ projrealisa/projectmanager/core/admin.py
@@ -7,3 +7,7 @@ admin.site.register(Phase)
 admin.site.register(Projet)
 admin.site.register(Organisme)
 admin.site.register(Employer)
+
+admin.site.site_header = "Gestion des Projets"
+admin.site.site_title = "Gestion des Projets Admin Portal"
+admin.site.index_title = "Bienvenue sur le portail de Gestion des Projets "
